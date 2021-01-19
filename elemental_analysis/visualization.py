0cf9b2fec275ce4d171f661acd7b3ef019de8316
Daniel Gafni <danielgafni16@gmail.com>
2021-01-19 14:04:16 +0300

    initial commit

---
+++ elemental_analysis/visualization.py
@@ -0,0 +1,16 @@
+import plotly.graph_objs as go
+
+
+def make_plotly_splom(data):
+    fig = go.Figure(
+        go.Splom(
+            dimensions = [
+                dict(label=colname, values=data[colname]) for colname in data.columns[1:]
+            ],
+            text=data["name"],
+        )
+    )
+
+    fig.update_layout(height=950, width=950, title_text="Elemental analysis methods")
+
+    return fig
