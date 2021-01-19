cf6fc40ff3aeb0f7ccee071956b1f8879e4d9b3f
Daniel Gafni <danielgafni16@gmail.com>
2021-01-19 14:56:05 +0300

    black

--- elemental_analysis/visualization.py
+++ elemental_analysis/visualization.py
@@ -4,8 +4,9 @@ import plotly.graph_objs as go
 def make_plotly_splom(data):
     fig = go.Figure(
         go.Splom(
-            dimensions = [
-                dict(label=colname, values=data[colname]) for colname in data.columns[1:]
+            dimensions=[
+                dict(label=colname, values=data[colname])
+                for colname in data.columns[1:]
             ],
             text=data["name"],
         )
